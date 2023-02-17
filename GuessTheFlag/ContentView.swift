3f152417a3406a79e5b4e881a386d4c414cac52b
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-17 15:03:06 -0600

    Initial commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,10 +7,159 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FlagImage: View {
+    let name: String
+    
     var body: some View {
-        Text("Hello, world!")
+        Image(name)
+            .renderingMode(.original)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .shadow(radius: 5)
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var questionCounter = 1
+    @State private var showingScore = false
+    @State private var showingResults = false
+    @State private var scoreTitle = ""
+    @State private var scoreNumber = 0
+    @State private var selectedFlag = -1
+    @State private var countries = allCountries.shuffled()
+    
+    static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
+    var body: some View {
+        ZStack {
+            
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 700, endRadius: 600)
+                .ignoresSafeArea()
+                      
+            VStack {
+                
+                Spacer()
+
+                Text("Guess The Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 23) {
+                    
+                    VStack {
+                        Text("Tap the country of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold)).italic()
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            FlagImage(name: countries[number])
+                                .rotation3DEffect(.degrees(selectedFlag == number ? 360 : 0), axis: (x: 0, y: 1, z: 0))
+                                .opacity(selectedFlag == -1 || selectedFlag == number ? 1 : 0.25)
+                                .scaleEffect(selectedFlag == -1 || selectedFlag == number ? 1 : 0.75)
+                                .saturation(selectedFlag == -1 || selectedFlag == number ? 1 : 0)
+                                .blur(radius: selectedFlag == -1 || selectedFlag == number ? 0 : 3)
+                                .animation(.default, value: selectedFlag)
+                                .accessibilityLabel(labels[countries[number], default: "Unknown Flag"])
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(scoreNumber)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+                
+            }
             .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(scoreNumber)")
+        }
+        .alert("Game Over!", isPresented: $showingResults) {
+            Button("Start Over", action: newGame)
+        } message: {
+            Text("Your final score was \(scoreNumber)")
+        }
+    }
+    func flagTapped(_ number: Int) {
+        selectedFlag = number
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            scoreNumber += 1
+        } else {
+            let needsThe = ["US", "UK"]
+            let theirAnswer = countries[number]
+            
+            if needsThe.contains(theirAnswer) {
+                scoreTitle = "False, that's the flag of the \(countries[number])."
+            } else {
+                scoreTitle = "False, that's the flag of \(countries[number])."
+            }
+            
+            
+            if scoreNumber > 0 {
+                scoreNumber -= 1
+            }
+            
+        }
+        if questionCounter == 8 {
+            showingResults = true
+        } else {
+            showingScore = true
+        }
+        
+    }
+    func askQuestion() {
+        countries.remove(at: correctAnswer)
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        questionCounter += 1
+        selectedFlag = -1
+        
+    }
+    func newGame() {
+        questionCounter = 0
+        scoreNumber = 0
+        countries = Self.allCountries
+        askQuestion()
     }
 }
 
@@ -18,4 +167,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
+ }
